2f901405db80722826805d52814010f3f328d937
chenlonglong <chenlonglong@qiyi.com>
2020-02-28 19:01:52 +0800

    add comment

--- include/global_func.h
+++ include/global_func.h
@@ -38,26 +38,41 @@ struct OC_PingStatus
 typedef void(*AsynCB)(bool, const char*);
 typedef void(*PingCallback)(const struct OC_PingStatus *status, int code, const char *msg);
 
+/**
+ * @brief 结构清零
+*/
 void ZeroPingStatus(struct OC_PingStatus *status);
 
+/**
+ * @brief 网络诊断包括ping、dns、tracerouter
+ * @param[in] host
+ * @param[in] dnsSer dns主机地址
+*/
 void OC_NetworkDiagnosis(const char* host, const char* dnsSer, PingCallback cb);
 
+/**
+ * @brief 网络诊断包括ping、dns、tracerouter
+ * @param[out] ipList 查到ip列表','分割
+*/
 int NativeGetHostNameByServer(const char* HostName, int timeout, const char* dnsServer, char* ipList);
 
-//sync call
+/**
+ * @brief 网络诊断包括ping、dns、tracerouter
+ * @param[in] times ping 次数
+ * @param[in] package_size探测包大小
+ * @param[out] result ping统计结果
+*/
 int NativePing(const char* host, int times, int package_size, int interval/*S*/, int timeout/*S*/, char* result);
 
-//sync call
 int NativeTraceRouter(const char* host, char* result);
 
-
-
+//sync call 回调在native线程
 int AsyncNativeGetHostNameByServer(const char* HostName, int timeout, const char* dnsServer, AsynCB cb);
 
-//sync call
+//sync call 回调在native线程
 int AsyncNativePing(const char* host, int times, int package_size, int interval/*S*/, int timeout/*S*/, AsynCB cb);
 
-//sync call
+//sync call 回调在native线程
 int AsyncNativeTraceRouter(const char* host, AsynCB cb);
 
 #ifdef __cplusplus
